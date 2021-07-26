4279b55f1b8c065f7ad8be520a11499490fe7a43
Masa <katsukatsu.masa.0522@gmail.com>
2021-07-26 23:23:47 +0900

    axios use

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import SearchBar from "./components/SearchBar";
+import ImageList from "./components/ImageList";
 
 const App = () => {
-  const onSearchSubmit = (term) => {
-    console.log(term);
+  const [images, setImages] = useState([]);
+  const onSearchSubmit = async (term) => {
+    try {
+      const params = {
+        key: "22648734-01451cea09d68fd778dab26ea",
+        q: term,
+      };
+      const response = await axios.get("https://pixabay.com/api/", { params });
+      setImages(response.data.hits);
+      if (response.data.total === 0) {
+        window.alert("お探しの画像はありません");
+      }
+    } catch {
+      window.alert("写真の取得に失敗しました");
+    }
   };
   return (
     <div className="ui container" style={{ marginTop: "20px" }}>
       <SearchBar onSubmit={onSearchSubmit} />
+      <ImageList images={images} />
     </div>
   );
 };
